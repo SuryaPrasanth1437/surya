9a637635edf4dd038b24b97feb43b43e86f95aff
SuryaPrasanth1437 <37958227+SuryaPrasanth1437@users.noreply.github.com>
2018-07-12 12:51:40 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,9 @@
-print("hi surya")
+print("Input")
+N=int(input())
+if N<=1 or N<=100000:
+    if N>0:
+        print("Positive")
+    elif N<0:
+        print("Negative")
+    else:
+        print("Zero")
